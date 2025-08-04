34fc4dd0035f9688af96b428634b92479bdada31
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-08-04 15:21:59 +0530

    Create dis-checker.js

---
+++ dis-checker.js
@@ -0,0 +1,9 @@
+let item =500;
+
+if(item>=1000){
+ let total =(item/100)*10;
+ let ans =item-total;
+ console.log("your total price is "+item +"\n" + "but here we discount it for you price is " +ans);
+}else{
+    console.log("your total price is "+item);
+}
