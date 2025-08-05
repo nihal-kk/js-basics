bd06ff8666a1ef70b7721d1b5ab4d0f81ca43aa6
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-08-05 07:09:57 +0530

    Create nonstop.js

---
+++ nonstop.js
@@ -0,0 +1,5 @@
+// this is non stop prompt code using while loop .when you not appleid value in the prompt that repet it under you add the value on prompt
+let b = '';
+while(b === null || b === ''){
+  b = prompt("can you share your weight")
+}
