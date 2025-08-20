7fd81bfff6cd4138db19b1d87a90d8d0f6bc1267
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-08-20 13:05:14 +0530

    Create repet-prompt.js

---
+++ repet-prompt.js
@@ -0,0 +1,8 @@
+  let n = prompt('hello');
+        if(n===null){
+            window.location.reload()
+        }else if(n === ""){
+            window.location.reload()
+        }else{
+            document.write('heelooo')
+        }
