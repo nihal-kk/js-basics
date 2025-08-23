09fd860bae6833a8c6413316ace3b52c50972bfc
Muhammad Nihal <muhammadnihal4662@gmail.com>
2025-08-23 16:35:18 +0530

    Create Array destructuring & Object destructuring.js

---
+++ Array destructuring & Object destructuring.js
@@ -0,0 +1,14 @@
+let [a, ,b,,c] =[1,2,3,4,5]
+console.log(a,b,c);
+
+let user = {
+  fname : "muhammed",
+  name :'nihal',
+  age :18,
+  domain : "mern"
+}
+
+let {name,domain} = user;
+
+console.log(name);
+console.log(domain);
